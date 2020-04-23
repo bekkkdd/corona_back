a8744ca282b741fd738b335d07077a2f48713d00
adiltyulyutaev <xynd@icloud.com>
2020-04-23 11:30:24 +0600

    added City Region and changed db

--- api/models.py
+++ api/models.py
@@ -22,9 +22,59 @@ class Country(models.Model):
         verbose_name_plural = 'Countries'
 
 
+class Region(models.Model):
+    country = models.ForeignKey(Country , on_delete=models.CASCADE,null=True)
+    name = models.CharField(max_length=300)
+    infected_count = models.IntegerField(default=0)
+    recovered_count = models.IntegerField(default=0)
+    died_count = models.IntegerField(default=0)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'coutry_id':self.country_id,
+            'name': self.name,
+            'infected_count': self.infected_count,
+            'recovered_count': self.recovered_count,
+            'died_count': self.died_count
+        }
+
+    class Meta:
+        verbose_name = 'Region'
+        verbose_name_plural = 'Regions'
+
+
+class City(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE,null = True)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=300)
+    infected_count = models.IntegerField(default=0)
+    recovered_count = models.IntegerField(default=0)
+    died_count = models.IntegerField(default=0)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'country_id':self.country_id,
+            'region_id':self.region_id,
+            'name': self.name,
+            'infected_count': self.infected_count,
+            'recovered_count': self.recovered_count,
+            'died_count': self.died_count
+        }
+
+    class Meta:
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+
+
+
+
 class Person(models.Model):
     name = models.CharField(max_length=300)
     surname = models.CharField(max_length=300)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, null=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, default=0)
     is_infected = models.NullBooleanField(blank=True, default=False)
     infected_by = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
@@ -41,6 +91,8 @@ class Person(models.Model):
             'id': self.id,
             'name': self.name,
             'surname': self.surname,
+            'region_id':self.region,
+            'city_id':self.city_id,
             'country_id': self.country_id,
             'is_infected': self.is_infected,
             'infected_by': self.infected_by,
--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Country, Person
+from api.models import Country, Person, City, Region
 
 
 class CountrySerializer(serializers.Serializer):
@@ -10,7 +10,6 @@ class CountrySerializer(serializers.Serializer):
     died_count = serializers.IntegerField()
 
     def create(self, validated_data):
-        # print("HEllo")
         country = Country.objects.create(
             name=validated_data.get('name'),
             infected_count=validated_data.get('infected_count'),
@@ -27,10 +26,42 @@ class CountrySerializer(serializers.Serializer):
         instance.save()
         return instance
 
+class RegionSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only='True')
+    name = serializers.CharField()
+    country = serializers.IntegerField()
+    infected_count = serializers.IntegerField()
+    recovered_count = serializers.IntegerField()
+    died_count = serializers.IntegerField()
+
+    def create(self, validated_data):
+        region = Region.objects.create(
+            name=validated_data.get('name'),
+            country_id=validated_data.get('country_id'),
+            infected_count=validated_data.get('infected_count'),
+            recovered_count=validated_data.get('recovered_count'),
+            died_count=validated_data.get('died_count')
+            )
+        return region
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.country_id = validated_data.get('country_id',instance.country_id)
+        instance.infected_count = validated_data.get('infected_count', instance.infected_count)
+        instance.recovered_count = validated_data.get('recovered_count', instance.recovered_count)
+        instance.died_count = validated_data.get('died_count', instance.died_count)
+        instance.save()
+        return instance
+
+class CitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = City
+        fields = ('id', 'name', 'country_id', 'region_id', 'infected_count',  'recovered_count', 'died_count')
+
 
 class PersonSerializer(serializers.ModelSerializer):
-    # country_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Person
-        fields = ('id', 'name', 'surname', 'is_infected', 'infected_by', 'country_id' ,'infected_date', 'is_recovered', 'is_died')
+        fields = ('id', 'name', 'surname', 'is_infected', 'infected_by', 'country_id','region_id','city_id','infected_date', 'is_recovered', 'is_died')
